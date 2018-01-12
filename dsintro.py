82e8e1125ad00bee052b943c54276cc403dba5f4
Hazel Apondi <apondihazel@gmail.com>
2018-01-12 15:14:32 +0300

    Delete dsintro.py

--- dsintro.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
-#%matplotlib inline  not valid Python code so we can't put it in the script. We'll put it in the first cell.
-
